bf127c3692a905f79dda38a118776f6ebddc6322
Fizxer21 <Fizxer290350@gmail.com>
2025-06-18 22:55:20 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,4 +1,12 @@
-array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [value for value in array if value >1]
-print(array)
-print(new_array)
+def main():
+    array = [2, 8, 9, 48, 8, 22, -12, 2]
+    result = []
+
+    for i in array:
+        if i not in result:
+            result.append(i)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
