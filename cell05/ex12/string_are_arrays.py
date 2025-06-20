4ec1b22dca51abe07ee9fe59b77e3154e72e62ac
Fizxer21 <Fizxer290350@gmail.com>
2025-06-20 14:21:06 +0700

    Create string_are_arrays.py

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,12 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    input_str = sys.argv[1]
+    count_z = input_str.count("z")
+
+    if count_z == 0:
+        print("none")
+    else:
+        print("z" * count_z)
